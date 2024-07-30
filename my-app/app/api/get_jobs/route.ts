9e2aee67f4dfed0f1ab47d9b252b5dc47a48d1cf
GitHubFaouaz <faouaza@yahoo.com>
2024-07-30 13:32:08 +0200

    welcom to the jungle

--- my-app/app/api/get_jobs/route.ts
+++ my-app/app/api/get_jobs/route.ts
@@ -10,23 +10,25 @@ export const GET = async (req: Request) => {
   // Lancer le navigateur
   const browser = await chromium.launch();
   // -------------------test-------------------
-  const page = await browser.newPage();
-  await page.goto(
-    "https://openclassrooms.com/fr/dashboard/employment/job-postings?learningPaths=185%7CD%C3%A9veloppeur%20Web"
-  );
-  const content = await page.$$eval("#mainContent ul li  ", (rows) =>
-    rows.map((li) => li.innerHTML)
-  );
+  // const page = await browser.newPage();
+  // await page.goto(
+  //   "https://www.welcometothejungle.com/fr/jobs?query=d%C3%A9veloppeur&refinementList%5Boffices.country_code%5D%5B%5D=FR"
+  // );
+
+  // const content = await page.$$eval(".sc-1udkli7-0  ul li  ", (items) =>
+  //   items.map((item) => item.innerHTML)
+  // );
 
   //------------------test---------------
 
   // const remotejobs = await getRemoteOkJobs(browser); // 1er scrapping
   // const workRemoteJobs = await getWorkRemotlyJobs(browser); // 2eme scrapping
-  // const workOpenClassRoom = await getWorkOpenClassRooms(browser); // 3eme scrapping
+  const workWelwomToTheJungle = await getWorkToTheJungle(browser); // 3eme scrapping
 
   // recuperation des 2 scrapping
   // const jobs = [...remotejobs, ...workRemoteJobs];
-  // const jobs = [...workOpenClassRoom];
+  const jobs = [...workWelwomToTheJungle];
+  console.dir(jobs);
 
   // Filtrer les valeurs undefined
   // const filteredJobs = jobs.filter((job) => job !== undefined);
@@ -43,8 +45,8 @@ export const GET = async (req: Request) => {
   return NextResponse.json({
     // remotejobs,//http://localhost:3000/api/get_jobs
     // workRemoteJobs,
-    // jobs,
-    content,
+    jobs,
+    // content,
   });
 };
 
@@ -179,68 +181,29 @@ const getWorkRemotlyJobs = async (instance: Browser) => {
   });
   return JobsFiltered;
 };
-const getWorkOpenClassRooms = async (instance: Browser) => {
+
+// reception enregistrement des works
+const getWorkToTheJungle = async (instance: Browser) => {
   const page = await instance.newPage();
   await page.goto(
-    "https://openclassrooms.com/fr/dashboard/employment/job-postings?learningPaths=185%7CD%C3%A9veloppeur%20Web"
+    "https://www.welcometothejungle.com/fr/jobs?query=d%C3%A9veloppeur&refinementList%5Boffices.country_code%5D%5B%5D=FR"
   );
 
   // on cible la class article li  de la page
-  const jobs = await page.$$eval("ul li", (rows) => {
-    return rows.map((row) => {
-      // pour eviter dafficher des pubs on filtre avant
-      // if (row.classList.contains("ad")) return; // on retourne que ce qui contient la classList ad
-      // ligne avec info par defaut a afficher
-      const obj = {
-        title: "",
-        company: "",
-        date: new Date(),
-        // logo: "",
-        salary: "",
-        url: "",
-      } as Jobs; // on se base sur le jobs du schema.prisma
+  const jobs = await page.$$eval(".sc-1udkli7-0  ul li .kkKAOM", (rows) =>
+    rows.map((li) => li.innerHTML)
+  );
 
-      // recuperation h2 de la page
-      const title = row.querySelector(".title"); // h2 de la page
-      if (title) {
-        obj.title = title.textContent?.trim() ?? "";
-      }
-      // recuperation de la company
-      const company = row.querySelector(".company");
-      if (company) {
-        obj.company = company.textContent?.trim() ?? "";
-      }
-
-      //recuperation de limage (logo)
-      // const divLogo = row.querySelector(".flag-logo") as HTMLDivElement; // de la div
-      // if (divLogo) {
-      //   const backgroundImage = divLogo.style.backgroundImage;
-      //   const img = backgroundImage
-      //     ?.replace("url(", "")
-      //     .replace(")", "")
-      //     .replace('"', "");
-      //   obj.logo = img;
-      // }
-
-      //recuperatiino de l'url
-      const aElement = row.querySelectorAll("a")[1];
-      if (aElement) {
-        obj.url =
-          "https://weworkremotely.com/" + aElement.getAttribute("href") ?? "";
-      }
-
-      return obj;
-    });
-  });
   // on filtre si les elements sont undefied
-  const JobsFiltered = jobs.filter((job) => {
-    if (!job) return false;
-    if (!job?.title) return false;
-    if (!job?.url) return false;
-    if (!job?.company) return false;
-    return true;
-  });
-  return JobsFiltered;
+  // const JobsFiltered = jobs.filter((job) => {
+  //   if (!job) return false;
+  //   if (!job?.title) return false;
+  //   if (!job?.url) return false;
+  //   if (!job?.company) return false;
+  //   return true;
+  // });
+  // return JobsFiltered;
+  return jobs;
 };
 
 // avec bright Data(site :https://brightdata.com/cp/zones/scraping_browser_faouaz/access_params) on va pouvoir scrapper pour recuperer les données dautres site
