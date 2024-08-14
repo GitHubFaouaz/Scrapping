d596dcb371f28b3f0894197c5ac0c464f6525491
GitHubFaouaz <faouaza@yahoo.com>
2024-08-14 13:37:26 +0200

    config tailwind

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -40,7 +40,8 @@ export default async function Home() {
                     // className="w-[90%] h-full object-cover"
                   />
                 </div>
-                <div className="flex flex-col gap-2 w-[100%] ">
+                <div className="flex flex-col gap-2 w-[100%] relative bg-[#000] custom-border-bl-radius">
+                  <div className="w-[5%] bg-red-500 absolute left-0 h-full custom-border-bl-radius"></div>
                   <CardHeader className="flex flex-row gap-4 p-2">
                     <Avatar>
                       <AvatarFallback>{j.company[0]}</AvatarFallback>
@@ -51,7 +52,7 @@ export default async function Home() {
                     <span>{j.company}</span>
                   </CardHeader>
 
-                  <CardTitle className="p-2 text-[20px] h-[60px]">
+                  <CardTitle className="p-2 text-[20px] h-[60px] z-10">
                     {j.title}
                   </CardTitle>
 
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,13 +1,17 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    // './pages/**/*.{ts,tsx}',
+    // './components/**/*.{ts,tsx}',
+    // './app/**/*.{ts,tsx}',
+    // './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx,js,jsx,html}",
+    "./components/**/*.{ts,tsx,js,jsx,html}",
+    "./app/**/*.{ts,tsx,js,jsx,html}",
+    "./src/**/*.{ts,tsx,js,jsx,html}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -75,6 +79,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
