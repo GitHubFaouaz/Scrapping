d6479cb18a50de027e38e1d4eba26ba822c86a6d
GitHubFaouaz <faouaza@yahoo.com>
2024-09-01 12:07:59 +0200

    custom ul

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -3,10 +3,6 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    // './pages/**/*.{ts,tsx}',
-    // './components/**/*.{ts,tsx}',
-    // './app/**/*.{ts,tsx}',
-    // './src/**/*.{ts,tsx}',
     "./pages/**/*.{ts,tsx,js,jsx,html}",
     "./components/**/*.{ts,tsx,js,jsx,html}",
     "./app/**/*.{ts,tsx,js,jsx,html}",
